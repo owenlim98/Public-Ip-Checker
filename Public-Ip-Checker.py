272b13b7b4d8fa84121ffba6697785473a0254eb
owenlim98 <32731852+owenlim98@users.noreply.github.com>
2022-01-22 20:00:15 +0800

    Optimised the code

--- Public-Ip-Checker.py
+++ Public-Ip-Checker.py
@@ -1,6 +1,8 @@
 from requests import get
 import os
 import webbrowser
+import pyperclip
+import selenium
 
 fileName = "PublicIp.txt"
 fileLocation = "F:\Minecraft Servers\PublicIp.txt"
@@ -12,27 +14,29 @@ def checkIfFileExist(fileLocation):
     return os.path.exists(fileLocation)
 
 def checkIfPublicIpStillTheSame(publicIp):
+    currentIp = fileOpen.read()
+    print("Current IP " + currentIp + "\nChecking if Public IP Changed")
+
     if currentIp != publicIp:
         print("Oops your public IP seems to have changed \nCurrent public IP is " + currentIp + "\nNew public IP is " + publicIp)
         webbrowser.open_new_tab("https://www.noip.com/")
         saveNewIP(publicIp)
+        pyperclip.copy(publicIp)
         print("Save new public IP to file!")
     
     else:
-        fileOpen.close()
         print("Current public IP still the same as before!")
+        
+    fileOpen.close()
 
 def saveNewIP(publicIp):
     fileOpen.write(publicIp)
-    fileOpen.close()
-    
 
 if checkIfFileExist(fileLocation):
+    print("File Exist!")
     fileOpen = open(fileLocation, "r+")
-    currentIp = fileOpen.read()
-    print("Current IP " + currentIp + "\nChecking if Public IP Changed")
     checkIfPublicIpStillTheSame(publicIp)
-
+    
 else:
     print("File does not exist, creating file now!")
     fileCreation = open(fileLocation, "w+")
